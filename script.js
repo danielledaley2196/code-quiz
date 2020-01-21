527760da7e27c9102d32df7b6bf57243a920d1ae
Danielle Daley <danielledaley2196@gmail.com>
2020-01-21 17:40:09 -0500

    added pseaudo code

--- script.js
+++ script.js
@@ -7,4 +7,18 @@
 // If answered incorrectly, time is subtracted
 // All questions answered (5) or timer gets to zero, game over
 // show score, and give option to save initials
-// Highscore hyperlink in to corner, when click shows all previous games, even when refreshed (use local storage), have option to clard local storage
+// Highscore hyperlink in to corner, when click shows all previous games, even when refreshed (use local storage), have option to clear local storage
+
+// array of questions longer than 5
+// array with answers
+// use brainstorm from letty
+// function for random questiomn and answers
+// 
+
+// event listener for start
+
+// event listener for question answered
+
+//event listener for input
+
+// function for randomquestion
