21bec5b48f24f5f92f7476af2205e26c0d73c92a
Danielle Daley <danielledaley2196@gmail.com>
2020-01-21 16:00:14 -0500

    pseudocode

---
+++ script.js
@@ -0,0 +1,10 @@
+// Click start button
+// Timer start
+// First question shown
+// User selects question from multiple choice 
+// Tell if the answer was correct or not quickly
+// Move onto next questions
+// If answered incorrectly, time is subtracted
+// All questions answered (5) or timer gets to zero, game over
+// show score, and give option to save initials
+// Highscore hyperlink in to corner, when click shows all previous games, even when refreshed (use local storage), have option to clard local storage
